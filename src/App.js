1e827733d1e7868ab8df8989055c5bbc138b2414
AyushFullStackDevelloper <158497283+AyushFullStackDevellope@users.noreply.github.com>
2025-03-20 16:18:07 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import {
   Routes,
   Route,
   Navigate,
+  useLocation,
 } from "react-router-dom";
 // =============================IMPORT COMPONENTS===============================================
 import Header from "./components/common/Header.js";
 import Home from "./components/Home";
-import "./App.css";
+import "./styles/App.css";
 import Services from "./components/Services";
 import TheData from "./components/TheData";
 import Customers from "./components/Customers";
@@ -16,14 +17,29 @@ import Posts from "./components/Posts";
 import Footer from "./components/common/Footer.js";
 import TempControll from "./components/TempControll";
 import Agreement from "./components/Agreement.js";
-import useScrollAnimation from "./components/useScrollAnimation";
+import useScrollAnimation from "./components/common/useScrollAnimation.js";
 import MriChillers from "./components/MriChillers";
-// import OurTechnology from "./components/OurTechnology";
+import OurTechnology from "./components/technology/OurTechnology.js";
 import Contact from "./components/Contact";
 import EquipmentBrand from "./components/EquipmentBrand.js";
 import ReqServices from "./components/ReqServices.js";
+import About from "./components/about/About.js";
+import BioSafety from "./components/BioSafety";
+import BioMedComponent from "./components/BioMed.js";
 
-// import About from "./components/About";
+
+// Keep using your original HashRouter to avoid breaking existing styles
+// Just add this simple scroll restoration function
+const ScrollToTop = () => {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [location]);
+  return null;
+};
 
 function MainLayout() {
   useScrollAnimation({ duration: 200, easing: "easeInOutQuad", offset: -80 });
@@ -61,25 +77,25 @@ function MainLayout() {
   return (
     <div className="App">
       <Header scrollToSection={scrollToSection} />
-      <div id="main" className="section-fade-in" >
+      <div id="main" className="section-fade-in">
         <Home />
       </div>
       {/* <div ref={homeRef} className="section-fade-in" dangerouslySetInnerHTML={{ __html: htmlContent }} /> */}
-      <div id="services" className="section-fade-in" >
+      <div id="services" className="section-fade-in">
         <Services />
       </div>
       <TheData />
       <div id="customers" className="section-fade-in">
         <Customers />
       </div>
+      {/* <div id="agreement" className="section-fade-in">
+        <Agreement />
+      </div> */}
       <div id="posts" className="section-fade-in">
         <Posts />
       </div>
-      <div id="agreement" className="section-fade-in">
-        <Agreement />
-      </div>
+      <br />
       <Footer />
-      
     </div>
   );
 }
@@ -88,13 +104,117 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route  path="/" element={<MainLayout />} />
-        <Route path="/temp-control" element={<TempControll />} />
-        <Route path="/mri-chillers" element={<MriChillers />} />
-        {/* <Route path="/our-technology" element={<About />} /> */}
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/equipment-brands" element={<EquipmentBrand />} />
-        <Route path="/request-service" element={<ReqServices />} /> 
+        <Route path="/" element={<MainLayout />} />
+        <Route
+          path="/temp-control"
+          element={
+            <>
+              <ScrollToTop />
+              <Header />
+              <TempControll />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/mri-chillers"
+          element={
+            <>
+              <ScrollToTop />
+              <Header />
+              <MriChillers />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <>
+              <ScrollToTop />
+              <Header />
+              <About />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            <>
+              <ScrollToTop />
+              <Header />
+              <Contact />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/equipment-brands"
+          element={
+            <>
+              <ScrollToTop />
+              <Header />
+              <EquipmentBrand />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/request-service"
+          element={
+            <>
+              <ScrollToTop />
+              <Header />
+              <ReqServices />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/bio-safety"
+          element={
+            <>
+              <ScrollToTop />
+              <Header />
+              <BioSafety />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/agreements"
+          element={
+            <>
+              <ScrollToTop />
+              <Header />
+              <Agreement />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/bio-med"
+          element={
+            <>
+              <ScrollToTop />
+              <Header />
+              <BioMedComponent />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/our-technology"
+          element={
+            <>
+              <ScrollToTop />
+              <Header />
+              <OurTechnology />
+              <Footer />
+            </>
+          }
+        />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
